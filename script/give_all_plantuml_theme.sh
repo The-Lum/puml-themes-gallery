9ae6750059e659156a683e1a3ca7a31a9201a566
The-Lum <86879521+The-Lum@users.noreply.github.com>
2021-10-30 17:07:24 +0200

    Update give_all_plantuml_theme.sh
    Add '_' char. (cf. _none_ theme)

--- script/give_all_plantuml_theme.sh
+++ script/give_all_plantuml_theme.sh
@@ -1,1 +1,1 @@
-echo -e "@startuml\nhelp themes\n@enduml" | java -jar lib/plantuml.jar -pipe -txt | awk '/^ {10,}[[:alnum:]]/ { print $1 }' > lib/all_plantuml_theme.txt
+echo -e "@startuml\nhelp themes\n@enduml" | java -jar lib/plantuml.jar -pipe -txt | awk '/^ {10,}[[:alnum:]_]/ { print $1 }' > lib/all_plantuml_theme.txt
