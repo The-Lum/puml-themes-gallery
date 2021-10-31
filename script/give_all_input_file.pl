4a84dd6e2e6a5e68d4cddca3c51d64db5e762013
The-Lum <86879521+The-Lum@users.noreply.github.com>
2021-10-31 11:45:10 +0100

    Create give_all_input_file.pl

---
+++ script/give_all_input_file.pl
@@ -0,0 +1,22 @@
+
+# Author: The-Lum
+# Script: 
+# The script generates the list of all the `.puml` input files
+# * list of all `.puml` files of the `input` directory
+# * without `path` and `ext`
+
+use strict;
+use warnings;
+use autodie;
+
+# Input file: all the .puml file
+my @files = <input/*.puml>;
+
+# Output file
+open(my $fho, '>:encoding(UTF-8)', '_data/input_files');
+
+foreach my $f (@files) {
+	(my $file_no_ext = $f) =~ s/\.[^.]+$//;
+	$file_no_ext =~ s|^.*/||;
+	print $fho $file_no_ext;
+}
