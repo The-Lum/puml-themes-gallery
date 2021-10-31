22b2c7c2bbd1dadf979ce8ad30b6f1f5f219df6f
The-Lum <86879521+The-Lum@users.noreply.github.com>
2021-10-31 10:52:07 +0100

    Update give_all_plantuml_theme.sh
    Put the output file on `_data` dir., to avoid to duplicate data for Jekyll.
    (See https://jekyllrb.com/docs/datafiles)

--- script/give_all_plantuml_theme.sh
+++ script/give_all_plantuml_theme.sh
@@ -1,1 +1,2 @@
-echo -e "@startuml\nhelp themes\n@enduml" | java -jar lib/plantuml.jar -pipe -txt | awk '/^ {10,}[[:alnum:]_]/ { print $1 }' > lib/all_plantuml_theme.txt
+mkdir _data
+echo -e "@startuml\nhelp themes\n@enduml" | java -jar lib/plantuml.jar -pipe -txt | awk '/^ {10,}[[:alnum:]_]/ { print $1 }' > _data/themes.yml
