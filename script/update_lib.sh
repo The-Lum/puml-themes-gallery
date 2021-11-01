2544f90ccae496d02ef3942740b65a2f4cca54e7
The-Lum <86879521+The-Lum@users.noreply.github.com>
2021-11-01 17:36:25 +0100

    Update update_lib.sh
    Task https://github.com/The-Lum/puml-themes-gallery/projects/1#card-71956891

--- script/update_lib.sh
+++ script/update_lib.sh
@@ -1,3 +1,8 @@
 mkdir -p lib
-curl -L https://sourceforge.net/projects/plantuml/files/plantuml.jar/download -o lib/plantuml.jar
+#curl -L https://sourceforge.net/projects/plantuml/files/plantuml.jar/download -o lib/plantuml.jar
+
+tag=$(curl -s https://api.github.com/repos/plantuml/plantuml/releases/latest | jq -r '.tag_name')
+echo $tag
+curl -L "https://github.com/plantuml/plantuml/releases/download/${tag}/plantuml-${tag#v}.jar" -o lib/plantuml.jar
+
 java -jar lib/plantuml.jar -version > lib/README.md
