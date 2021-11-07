219b333fb8560ef767ed117825788c186eab9be6
The-Lum <86879521+The-Lum@users.noreply.github.com>
2021-11-07 20:01:30 +0100

    Update update_lib.sh

--- script/update_lib.sh
+++ script/update_lib.sh
@@ -1,9 +1,9 @@
 mkdir -p lib
-rm lib/plantuml.jar
+#rm lib/plantuml.jar
 #curl -L https://sourceforge.net/projects/plantuml/files/plantuml.jar/download -o lib/plantuml.jar
 
 tag=$(curl -s https://api.github.com/repos/plantuml/plantuml/releases/latest | jq -r '.tag_name')
-echo $tag > lib/plantuml-tag.md
+echo -n $tag > lib/plantuml-tag.md
 curl "https://github.com/plantuml/plantuml/releases/download/${tag}/plantuml-${tag#v}.jar" -L -o lib/plantuml.jar
 
 #java -jar lib/plantuml.jar -version > lib/README.md
