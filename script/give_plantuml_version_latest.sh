a82107c8cad2b56007a4194c42b8ee7ac39ecf86
The-Lum <86879521+The-Lum@users.noreply.github.com>
2021-11-07 19:11:32 +0100

    Update give_plantuml_version_latest.sh

--- script/give_plantuml_version_latest.sh
+++ script/give_plantuml_version_latest.sh
@@ -1,3 +1,3 @@
 tag=$(curl -s https://api.github.com/repos/plantuml/plantuml/releases/latest | jq -r '.tag_name')
 echo -n $tag > lib/plantuml-tag-latest.md
-echo "::notice plantuml-tag-latest: $tag"
+echo "::notice:: plantuml-tag-latest: $tag"
