f22d1ab3e8703976712ea3ff8302326400de1552
Johannes Thorn <2544827+johthor@users.noreply.github.com>
2024-11-01 19:40:32 +0100

    Move generated diagrams into assets and exclude generated PUML files from site
    Issue: #3

--- script/generate_all_SVG.sh
+++ script/generate_all_SVG.sh
@@ -1,3 +1,3 @@
-java -jar lib/plantuml.jar -stdrpt -nometadata -duration -Playout=smetana -svg -o img gallery
+java -jar lib/plantuml.jar -stdrpt -nometadata -duration -Playout=smetana -svg -o ../assets/diagrams gallery
 echo "End"
 exit 0
