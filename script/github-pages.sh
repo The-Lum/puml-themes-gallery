248336c5dbe24cc8170bc387852f5b14c92e6348
Johannes Thorn <2544827+johthor@users.noreply.github.com>
2024-11-10 18:10:09 +0100

    Add test function for correct link and source structure
    Issue: #3

--- script/github-pages.sh
+++ script/github-pages.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
-strContains() { case $1 in *$2* ) return 0;; *) return 1;; esac ;}
-
 build() {
   JEKYLL_ENV=production bundle exec jekyll build
 }
 
 serve() {
   bundle exec jekyll serve --livereload --incremental --baseurl=""
+}
+
+test() {
+  PAGES_REPO_NWO=The-Lum/puml-themes-gallery JEKYLL_ENV=production bundle exec jekyll build --baseurl="/fizzbuzz"
+
+  echo "Found bad links:"
+  grep -r --exclude='*.svg' 'href=' _site | grep -v -e 'href="http' -e 'href="/fizzbuzz' -e 'markdown-toc' > findings.txt
+
+  echo "Found bad image sources:"
+  grep -r --exclude='*.svg' 'src=' _site | grep -v -e 'src="/fizzbuzz' -e 'src="//html5'
 }
 
 clean() {
